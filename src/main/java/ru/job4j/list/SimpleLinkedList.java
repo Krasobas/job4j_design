4e5cb763b311ff791cf3e181e194400d36313c2c
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-10-20 13:20:01 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/list/SimpleLinkedList.java
@@ -0,0 +1,99 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int size;
+    private int modCount;
+    private Node<E> first;
+    private Node<E> last;
+
+    private void linkLast(E element) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(last, element, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    private Node<E> node(int index) {
+        Node<E> result = first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result;
+    }
+
+    @Override
+    public void add(E value) {
+        linkLast(value);
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        return node(index).item;
+    }
+
+    @Override
+    public E set(int index, E newValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            Node<E> result;
+            private int point = 0;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                result = point == 0 ? first : result.next;
+                point++;
+                return result.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/list/SimpleLinkedListTest.java
@@ -0,0 +1,135 @@
+package ru.job4j.list;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class SimpleLinkedListTest {
+    List<Integer> list;
+
+    @Before
+    public void initData() {
+        list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThenSizeIncrease() {
+        Assert.assertEquals(3, list.size());
+    }
+
+    @Test
+    public void whenAddNullThenMustBeSameBehavior() {
+        list = new SimpleLinkedList<>();
+        list.add(null);
+        list.add(null);
+        Assert.assertEquals(2, list.size());
+        Assert.assertNull(list.get(0));
+        Assert.assertNull(list.get(1));
+    }
+
+    @Test
+    public void whenAddAndGet() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        assertThat(list.get(0), Is.is(1));
+        assertThat(list.get(1), Is.is(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetFromOutOfBoundThenExceptionThrown() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.get(2);
+    }
+
+    @Test
+    public void whenAddIterHasNextTrue() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), Is.is(true));
+    }
+
+    @Test
+    public void whenAddIterNextOne() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.next(), Is.is(1));
+    }
+
+    @Test
+    public void whenEmptyIterHashNextFalse() {
+        List<Integer> list = new SimpleLinkedList<>();
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), Is.is(false));
+    }
+
+    @Test
+    public void whenAddIterMultiHasNextTrue() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), Is.is(true));
+        assertThat(it.hasNext(), Is.is(true));
+    }
+
+    @Test
+    public void whenAddIterNextOneNextTwo() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.next(), Is.is(1));
+        assertThat(it.next(), Is.is(2));
+    }
+
+    @Test
+    public void whenGetIteratorTwiceThenEveryFromBegin() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        Iterator<Integer> first = list.iterator();
+        assertThat(first.hasNext(), Is.is(true));
+        assertThat(first.next(), Is.is(1));
+        assertThat(first.hasNext(), Is.is(true));
+        assertThat(first.next(), Is.is(2));
+        assertThat(first.hasNext(), Is.is(false));
+        Iterator<Integer> second = list.iterator();
+        assertThat(second.hasNext(), Is.is(true));
+        assertThat(second.next(), Is.is(1));
+        assertThat(second.hasNext(), Is.is(true));
+        assertThat(second.next(), Is.is(2));
+        assertThat(second.hasNext(), Is.is(false));
+    }
+
+    @Test
+    public void whenCheckIterator() {
+        Iterator<Integer> iterator = list.iterator();
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(1), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(2), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(3), iterator.next());
+        Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenAddAfterGetIteratorThenMustBeException() {
+        Iterator<Integer> iterator = list.iterator();
+        list.add(4);
+        iterator.next();
+    }
+}
