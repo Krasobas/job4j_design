ee52703dc058faa710980856193a9ed6d3c04c9b
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-10-25 08:53:15 +0300

    7. ListIterator [#350217 #219593] Исправление №3

--- src/main/java/ru/job4j/it/ListUtils.java
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -12,28 +12,23 @@ public class ListUtils {
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iter = list.listIterator(index);
-        iter.next();
-        iter.add(value);
+        list.listIterator(++index).add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (filter.test(i.next())) {
-                i.previous();
-                i.remove();
+        ListIterator<T> iter = list.listIterator();
+        while (iter.hasNext()) {
+            if (filter.test(iter.next())) {
+                iter.remove();
             }
         }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (filter.test(i.next())) {
-                i.previous();
-                i.set(value);
-                i.next();
+        ListIterator<T> iter = list.listIterator();
+        while (iter.hasNext()) {
+            if (filter.test(iter.next())) {
+                iter.set(value);
             }
         }
     }
@@ -43,7 +38,6 @@ public class ListUtils {
             ListIterator<T> src = list.listIterator();
             while (src.hasNext()) {
                 if (src.next() == element) {
-                    src.previous();
                     src.remove();
                 }
             }
--- src/test/java/ru/job4j/it/ListUtilsTest.java
+++ src/test/java/ru/job4j/it/ListUtilsTest.java
@@ -10,74 +10,78 @@ import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.*;
 
 public class ListUtilsTest {
-    List<Integer> input;
-
-    @Before
-    public void setUp() {
-        input = new ArrayList<>(Arrays.asList(1, 2, 3));
-    }
 
     @Test
     public void whenAddBefore() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.addBefore(input, 1, 4);
         assertThat(input, is(List.of(1, 4, 2, 3)));
     }
 
     @Test
     public void whenAddBeforeAndIndexZero() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.addBefore(input, 0, 4);
         assertThat(input, is(List.of(4, 1, 2, 3)));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenAddBeforeWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.addBefore(input, 3, 4);
     }
 
     @Test
     public void whenAddAfterLast() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.addAfter(input, 2, 4);
         assertThat(input, is(List.of(1, 2, 3, 4)));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenAddAfterInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.addAfter(input, 3, 4);
     }
 
     @Test
     public void whenThenRemoveIfAndPredicateFalse() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.removeIf(input, i -> i < 0);
         assertThat(input, is(List.of(1, 2, 3)));
     }
 
     @Test
     public void whenThenRemoveIfAndPredicateTrue() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.removeIf(input, i -> i < 3);
         assertThat(input, is(List.of(3)));
     }
 
     @Test
     public void whenThenReplaceIfAndPredicateFalse() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.replaceIf(input, i -> i < 0, 0);
         assertThat(input, is(List.of(1, 2, 3)));
     }
 
     @Test
     public void whenThenReplaceIfAndPredicateTrue() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.replaceIf(input, i -> i < 3, 0);
         assertThat(input, is(List.of(0, 0, 3)));
     }
 
     @Test
     public void whenRemoveAllAndMatch() {
-        input = new ArrayList<>(List.of(1, 3, 4, 1, 5, 0, 4));
+        List<Integer> input = new ArrayList<>(List.of(1, 3, 4, 1, 5, 0, 4));
         ListUtils.removeAll(input, List.of(0, 1, 4));
         assertThat(input, is(List.of(3, 5)));
     }
 
     @Test
     public void whenRemoveAllAndDoNotMatch() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 3));
         ListUtils.removeAll(input, List.of(4, 5));
         assertThat(input, is(List.of(1, 2, 3)));
     }
