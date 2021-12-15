b1beb349878e5b7c0de012ec10f9ed61474bd466
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-12-15 15:02:53 +0300

    5. Валидация параметров запуска. [#246865] Повтор

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,35 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<Path>> files = new HashMap<>();
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty key = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        if (!files.containsKey(key)) {
+            files.put(key, new ArrayList<>(List.of(file)));
+        } else {
+            files.get(key).add(file);
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    public Map<FileProperty, List<Path>> getDuplicates() {
+        Map<FileProperty, List<Path>> duplicates = new HashMap<>();
+        for (Map.Entry<FileProperty, List<Path>> entry : files.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                duplicates.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return duplicates;
+    }
+}
