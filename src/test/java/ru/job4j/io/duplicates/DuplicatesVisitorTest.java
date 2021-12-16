5a006dabcd58bc25958f0f3e20e4d225200ec678
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-12-16 12:02:42 +0300

    5. Валидация параметров запуска. [#246865] Исправление №4

--- src/test/java/ru/job4j/io/duplicates/DuplicatesVisitorTest.java
+++ src/test/java/ru/job4j/io/duplicates/DuplicatesVisitorTest.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -31,19 +30,20 @@ public class DuplicatesVisitorTest {
         assertTrue(visitor.getDuplicates().isEmpty());
     }
 
+    @Rule
+    public TemporaryFolder folder2 = new TemporaryFolder();
+
     @Test
     public void whenSomeDuplicates() throws IOException {
-        File firstParent = folder.newFolder("firstParent");
-        File firstChild = folder.newFile("firstParent/firstChild");
-        File secondChild = folder.newFile("firstParent/secondChild");
-        File secondParent = folder.newFolder("secondParent");
-        File secondChildDuplicate = folder.newFile("secondParent/secondChild");
+        File firstParent = folder2.newFolder("firstParent");
+        File firstChild = folder2.newFile("firstParent/firstChild");
+        File secondChild = folder2.newFile("firstParent/secondChild");
+        File secondParent = folder2.newFolder("secondParent");
+        File secondChildDuplicate = folder2.newFile("secondParent/secondChild");
         DuplicatesVisitor visitor = new DuplicatesVisitor();
-        Files.walkFileTree(folder.getRoot().toPath(), visitor);
+        Files.walkFileTree(folder2.getRoot().toPath(), visitor);
         Map<FileProperty, List<Path>> duplicates = visitor.getDuplicates();
-        List<Path> expected = new ArrayList<>();
-        expected.add(secondChild.toPath());
-        expected.add(secondChildDuplicate.toPath());
+        List<Path> expected = List.of(secondChild.toPath(), secondChildDuplicate.toPath());
         assertThat(visitor.getDuplicates().values().size(), is(1));
         assertTrue(duplicates.containsValue(expected));
     }
