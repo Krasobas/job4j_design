275da6b48de59594b6024b14c82c75df3ba70c0e
Vasiliy Krasov <vasiliy.krasov@gmail.com>
2021-12-16 15:24:19 +0300

    5. Валидация параметров запуска. [#246865 #240299] Удалил второй тест.

--- src/test/java/ru/job4j/io/duplicates/DuplicatesVisitorTest.java
+++ src/test/java/ru/job4j/io/duplicates/DuplicatesVisitorTest.java
@@ -30,19 +30,4 @@ public class DuplicatesVisitorTest {
         Files.walkFileTree(folder.getRoot().toPath(), visitor);
         assertTrue(visitor.getDuplicates().isEmpty());
     }
-    @Ignore
-    @Test
-    public void whenSomeDuplicates() throws IOException {
-        File firstParent = folder.newFolder("firstParent");
-        File firstChild = folder.newFile("firstParent/firstChild");
-        File secondChild = folder.newFile("firstParent/secondChild");
-        File secondParent = folder.newFolder("secondParent");
-        File secondChildDuplicate = folder.newFile("secondParent/secondChild");
-        DuplicatesVisitor visitor = new DuplicatesVisitor();
-        Files.walkFileTree(folder.getRoot().toPath(), visitor);
-        Map<FileProperty, List<Path>> duplicates = visitor.getDuplicates();
-        List<Path> expected = List.of(secondChild.toPath(), secondChildDuplicate.toPath());
-        assertThat(visitor.getDuplicates().values().size(), is(1));
-        assertTrue(duplicates.containsValue(expected));
-    }
 }
